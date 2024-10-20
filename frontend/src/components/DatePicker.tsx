042202d881e8c001ad06212e33679ac209837928
Jenna-Deamer <200529678@student.georgianc.on.ca>
2024-10-20 18:11:58 -0400

    Added min/max, styles, padding to transactions container

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -1,37 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers";
+import { Box } from "@mui/material";
+import "../styles/DatePicker.css";
+import dayjs from "dayjs";
 
 function DatePicker() {
+  const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(null);
+  // Set the min and max dates for the date picker
+  const minDate = dayjs(new Date(2020, 0, 1));
+  // Set the maximum date to December 31 of the current year
+  const maxDate = dayjs(new Date(new Date().getFullYear(), 11, 31));
+
   return (
-    <>
+    <Box className="date-picker-container">
       <MuiDatePicker
         label={"Change Date"}
         views={["month", "year"]}
-        sx={{
-          "& .MuiInputBase-root": {
-            backgroundColor: "#316380",
-            color: "#FFF",
-            width: "200px",
-            marginRight: "24px",
-          },
-          "& .MuiInputBase-input": {
-            color: "#FFF",
-          },
-          "& .MuiInputLabel-root": {
-            color: "#FFF",
-            "&.Mui-focused": {
-              color: "#FFF",
-            },
-          },
-          "& .MuiFormHelperText-root": {
-            color: "#FFF",
-          },
-          "& .MuiSvgIcon-root": {
-            color: "#FFF",
-          },
-        }}
+        value={selectedDate}
+        onChange={(newValue) => setSelectedDate(newValue)}
+        minDate={minDate}
+        maxDate={maxDate}
       />
-    </>
+    </Box>
   );
 }
 
