3874f08007771ed6eddf50030f06aca42f3faeed
Jenna Deamer <jennadeamer@gmail.com>
2024-10-21 14:52:06 -0400

    MUI MM/YYYY picker added with basic styles. Fixed Transaction padding, added Date state to app.tsx

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -1,16 +1,28 @@
+import React, { useState } from "react";
+import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers";
+import { Box } from "@mui/material";
+import "../styles/DatePicker.css";
+import dayjs from "dayjs";
 
 function DatePicker() {
+  const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(null);
+  // Set the min and max dates for the date picker
+  const minDate = dayjs(new Date(2020, 0, 1));
+  // Set the maximum date to December 31 of the current year
+  const maxDate = dayjs(new Date(new Date().getFullYear(), 11, 31));
 
-    //Pass date state from app
-    return (
-        <>
-            <button
-                className="secondary-button button"
-                title="Change month/year"
-            >
-                Change Date <i className="bi bi-calendar-fill"></i>
-            </button>
-        </>
-    )
+  return (
+    <Box className="date-picker-container">
+      <MuiDatePicker
+        label={"Change Date"}
+        views={["month", "year"]}
+        value={selectedDate}
+        onChange={(newValue) => setSelectedDate(newValue)}
+        minDate={minDate}
+        maxDate={maxDate}
+      />
+    </Box>
+  );
 }
-export default DatePicker;  
+
+export default DatePicker;
