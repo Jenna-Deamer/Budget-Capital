e457bb5311c8dcb8b114b8fb6a957a3e54b6f798
Jenna Deamer <jennadeamer@gmail.com>
2025-02-25 19:18:09 -0500

    Add Category Fetch to context

--- frontend/src/context/CategoryContext.tsx
+++ frontend/src/context/CategoryContext.tsx
@@ -3,5 +3,33 @@ import axios from "axios";
 
 const CategoryContext = createContext(null);
 
-
 export const useCategories = () => useContext(CategoryContext);
+
+export const CategoryProvider = ({ children }) => {
+    const [categories, setCategories] = useState([]);
+    const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+    
+    // Fetch all categories
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try{
+                const token = localStorage.getItem("jwtToken");
+                const response = await axios.get(`${API_URL}/category/categories`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                setCategories(response.data);
+            } catch (error) {
+                console.error("Error fetching categories: ", error);
+            }
+        };
+        fetchCategories();
+    }, [API_URL]);
+
+    return (
+        <CategoryContext.Provider value={{ categories, addCategory, updateCategory, deleteCategory }}>
+            {children}
+        </CategoryContext.Provider>
+    );
+};
